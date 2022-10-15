e73049796cf84e8f6dacbecb954d1a8ee8b3c7bb
Maxim-LD <ayinde779@gmail.com>
2022-10-14 21:07:13 -0700

    prints single digit base 10 numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - program that prints all single digit numbers of base 10 starting from
- * You are not allowed to use any variable of type char
- * You can only use the putchar function
- * Return: 0
+ * main - print single digit numbers starting from 0
+ *
+ * Return: returns zero ar the end
  */
-
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-putchar("\n");
-return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
 }
