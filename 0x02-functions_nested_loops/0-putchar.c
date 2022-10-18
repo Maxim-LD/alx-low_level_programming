696fc6c12a66c8039cfa85fddf0f591abdb2bed7
Maxim-LD <ayinde779@gmail.com>
2022-10-18 04:00:53 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the word "_putchar"
+ *
+ * return - 0
+ */
+
+int main() 
+
+{
+	putchar("_putchar\n");
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Contains nested loops
