272938c54ed6aa17009476803cd680e05ff27542
Maxim-LD <ayinde779@gmail.com>
2022-10-19 04:37:05 -0700

    checks if character is uppercase or lowercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,14 @@
 
 /**
  * _isalpha - return 1 if c is a letter(lowercase or uppercase)
- * 
  * @c: character examined
  *
- * return: 1 for satisfying the condition and 0 if otherwise
+ * Return: 1 for satisfying the condition and 0 if otherwise
  */
 
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	return (1);
-
 	return (0);	
 }
