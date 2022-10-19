f7ecd4879fdc859328daf06af8b403c0b00cf560
Maxim-LD <ayinde779@gmail.com>
2022-10-19 09:46:18 -0700

    prints the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ *
+ *@p: character used
+ *
+ * Return: last digit or nothing
+ */
+
+int print_last_digit(int p)
+{
+        int k;
+
+	k = p % 10;
+
+        if (k < 0)
+        {
+                _putchar(-k + '0');
+                return -k;
+        }
+	else
+	{
+		_putchar(k + '0');
+		return k;
+	}
+}
