2464ddb0b05d9bb24a3c0c4bce96b49dd998132d
Maxim-LD <ayinde779@gmail.com>
2022-10-19 16:37:58 -0700

    prints the all say clock

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,34 @@
 void jack_bauer(void)
 {
 	int m, h;
+
 	for (h = 0; h <= 23; h++)
 	{
-		for (m =0; m <= 59; m++);
+	for (m = 0; m <= 59; m++);
+
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-		        _putchar(10);
-		}	
+			if (h < 10)
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else if (h >= 10)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+			}
+			if (m < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(m + '0');
+			}
+			else if (m > 10)
+			{
+				_putchar(':');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+		}
 	}
 }
