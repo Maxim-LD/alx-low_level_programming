fadbf8107b5055267c0e41be1cb0ca93324cf830
Maxim-LD <ayinde779@gmail.com>
2022-10-19 20:50:08 -0700

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - the program adds two integer and return a new line
+ *
+ * @a: my guy
+ * @b: my other guy
+ *
+ * Return: returns
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
