6f7f1c09bf18a7bc65e0b2ed03036126cdf2aac7
Maxim-LD <ayinde779@gmail.com>
2022-10-26 15:01:23 -0700

    converts string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -15,8 +15,8 @@ char *string_toupper(char *n)
 	p = 0;
 	while (n[p] != '\0')
 	{
-	if (n[p] >= 'a' && n[p] <= 'z')
-		n[p] = n[p] - 32;
+		if (n[p] >= 'a' && n[p] <= 'z')
+			n[p] = n[p] - 32;
 		p++;
 	}
 	return (n);
