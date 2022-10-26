d4b925d6bdd8356c6bc79fd0c2e0db9be642921b
Maxim-LD <ayinde779@gmail.com>
2022-10-26 14:50:27 -0700

    convert string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercas
+ *
+ * @n: pointer
+ *
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int p;
+
+	p = 0;
+	while (n[p] != '\0')
+	{
+	if (n[p] >= 'a' && n[p] <= 'z')
+		n[p] = n[p] - 32;
+		p++;
+	}
+	return (n);
+}
