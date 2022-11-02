e3f4cf40fa197883a7fdf943ef783613f29ea45c
Maxim-LD <ayinde779@gmail.com>
2022-11-02 14:36:01 -0700

    raise a value to the power of another

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the value of a variable
+ * to power of another
+ *
+ * @x: given variable
+ * @y: the exponent
+ *
+ * Result: x exponential y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (0);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
