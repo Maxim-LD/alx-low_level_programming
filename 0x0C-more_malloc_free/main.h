dc79a2725fcbed9a0b6122e794364f4257831bee
Maxim-LD <ayinde779@gmail.com>
2022-11-10 13:08:09 -0800

    hot fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
