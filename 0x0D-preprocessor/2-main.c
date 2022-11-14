876ddc5d33b9458149fce55877f2d6541b637086
Maxim-LD <ayinde779@gmail.com>
2022-11-14 12:18:18 -0800

    hot fix

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the name of the current file
+*
+* Return: 0 and a new line
+*/
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+}
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define _FILE_
 
 #define SIZE 1024
 #define PI 3.14159265359
+#define ABS(x) (x)
 
 #endif
